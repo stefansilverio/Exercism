db9da384f43e6a2351563c3d9009d7060a75fca9
stefan silverio <stefansilverio@gmail.com>
2021-11-05 16:05:46 -0400

    Use scoped enums

--- cpp/triangle/triangle.cpp
+++ cpp/triangle/triangle.cpp
@@ -1,26 +1,32 @@
 #include "triangle.h"
 #include <iostream>
+using namespace std;
 
 namespace triangle {
 	
-	bool valid(double a, double b, double c)
+	bool obeys_inequality(double a, double b, double c)
 	{
-		if ((a + b > c) && (b + c > a) && (a + c > b))
-			return true; // sum of any two sides is greater than the third 
-		return false;
+		return ((a + b >= c) && (b + c >= a) && (a + c >= b));
 	}
 
 	flavor kind(double a, double b, double c)
 	{
-		if (a <= 0 || b <= 0 || c <= 0)
+		if (abs(a) <= 0 || abs(b) <= 0 || abs(c) <= 0)
 			throw std::domain_error("Triangles must have non-zero sides.");
-		if (!valid(a, b, c))
+		if (!obeys_inequality(a, b, c))
 			throw std::domain_error("Any two sides must sum to be greater than third.");
-		if (a == b && b == c)
-			return triangle::flavor::equilateral;
-		if (a == b || b == c || a == c)
-			return triangle::flavor::isosceles;
-		return triangle::flavor::scalene;
+		if ((abs(a-b) < 1e-9) && (abs(b-c) < 1e-9))
+		{
+			cout << "I'm equilateral\n";
+			return flavor::equilateral;
+		}
+		if ((abs(a-b) < 1e-9) || (abs(b-c) < 1e-9) || (abs(a-c) < 1e-9))
+		{
+		   cout << "I'm equilat\n";
+			return flavor::isosceles;
+		}
+		cout << "I'm scalene\n";
+		return flavor::scalene;
 	}
 
 }  // namespace triangle
